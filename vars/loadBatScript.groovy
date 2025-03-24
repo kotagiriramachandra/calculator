fdf20d7e817279d6f78e83592b7955f20c019c4a
kotagiriramachandra <97685796+kotagiriramachandra@users.noreply.github.com>
2025-03-24 19:58:48 -0400

    Update loadBatScript.groovy

--- vars/loadBatScript.groovy
+++ vars/loadBatScript.groovy
@@ -1,5 +1,5 @@
 def call(Map config = [:]) {
   def scriptcontents = libraryResource "scripts/windows/${config.name}"
-  witeFile file: "${config.name}", text: scriptcontents
+  writeFile file: "${config.name}", text: scriptcontents
   bat "./${config.name}"
 }
