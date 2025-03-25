31adfa679ac482e10ee18e64e9c3ddfc34986f40
kotagiriramachandra <97685796+kotagiriramachandra@users.noreply.github.com>
2025-03-24 20:31:10 -0400

    Update helloWorldExternal.groovy

--- vars/helloWorldExternal.groovy
+++ vars/helloWorldExternal.groovy
@@ -1,4 +1,4 @@
 def call(Map config = [:]) {
   loadBatScript(name: 'hello_world.bat')
-  bat 'hello_world.bat ${config.name} ${config.dayOfWeek}'
+  bat 'hello_world.bat %config.name %config.dayOfWeek'
 }
